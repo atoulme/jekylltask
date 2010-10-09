0ab56fc6ff1c4749b141aa2f7a24cd404f0dfbac
Peter Donald <peter@realityforge.org>
2010-10-10 10:39:27 +1100

    Push the creation of tasks down into the generate method so that the "auto" version will run even if there is no changes to source files

--- lib/jekylltask.rb
+++ lib/jekylltask.rb
@@ -17,58 +17,61 @@ require 'rake/tasklib'
 require 'jekyll'
 
 class JekyllTask < Rake::TaskLib
-  def initialize(name)  # :yield: self
+  def initialize(name) # :yield: self
     @name = name
     @source = nil
     @target = nil
     @auto = false
+    @description = nil
     yield self if block_given?
     raise "User must specify source directory" unless self.source
     raise "User must specify target directory" unless self.target
-    task name, :auto, :needs=>[@source] do |task, args|
-      generate
-    end
-    if @source != @target
-      file @target=>FileList["#{@source}/**/*"] do
-        generate
-      end
-      task 'clobber' do
-        rm_rf @target
-      end
-    end
+    generate
   end
 
   attr_reader :name
   attr_accessor :source
   attr_accessor :target
   attr_accessor :auto
+  attr_accessor :description
+
+  private
 
   def generate
+    desc self.description if self.description
     if self.auto
-      require 'directory_watcher'
-      puts "Auto generating: just edit a page and save, watch the console to see when we're done regenerating pages"
-      dw = DirectoryWatcher.new(self.source)
-      dw.interval = 1
-      dw.glob = Dir.chdir(self.source) do
-        dirs = Dir['*'].select { |x| File.directory?(x) }
-        dirs -= [target]
-        dirs = dirs.map { |x| "#{x}/**/*" }
-        dirs += ['*']
+      task self.name do
+        require 'directory_watcher'
+        puts "Auto generating: just edit a page and save, watch the console to see when we're done regenerating pages"
+        dw = DirectoryWatcher.new(source)
+        dw.interval = 1
+        dw.glob = Dir.chdir(source) do
+          dirs = Dir['*'].select { |x| File.directory?(x) }
+          dirs -= [target]
+          dirs = dirs.map { |x| "#{x}/**/*" }
+          dirs += ['*']
+        end
+        dw.start
+        site = init_jekyll
+        dw.add_observer do |*args|
+          t = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+          puts "[#{t}] regeneration: #{args.size} files changed"
+          site.process
+          puts "Done"
+        end
+        loop { sleep 1 }
       end
-      dw.start
-      site = init_jekyll
-      dw.add_observer do |*args|
-        t = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-        puts "[#{t}] regeneration: #{args.size} files changed"
-        site.process
-        puts "Done"
-      end
-      loop { sleep 1 }
     else
-      puts "Generating documentation in #{self.target}"
-      site = init_jekyll
-      site.process
-      touch self.target
+      task self.name => self.target
+      file self.target => FileList["#{self.source}/**/*"] do
+        puts "Generating documentation in #{target}"
+        site = init_jekyll
+        site.process
+        touch target
+      end
+    end
+    task 'clobber' do
+      rm_rf self.target
     end
   end
 
