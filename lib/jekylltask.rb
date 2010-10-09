625a94fcc3ff6306b89803766e82a46762313819
Peter Donald <peter@realityforge.org>
2010-10-10 10:42:07 +1100

    Suppress some duplicate constant warnings that occur latest version of liquid

--- lib/jekylltask.rb
+++ lib/jekylltask.rb
@@ -14,6 +14,18 @@
 # the License.
 
 require 'rake/tasklib'
+
+module Kernel
+  def suppress_warnings
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    result = yield
+    $VERBOSE = original_verbosity
+    return result
+  end
+end
+
+suppress_warnings { require 'liquid' }
 require 'jekyll'
 
 class JekyllTask < Rake::TaskLib
